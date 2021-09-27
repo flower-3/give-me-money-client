60dbc6daac6a801a5855ee4d447d0b756be32ad7
youngUnPark <mduddns@naver.com>
2021-09-27 23:53:54 +0900

    chore: rca, material ui 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button variant="contained">flower-3</Button>;
         <a
           className="App-link"
           href="https://reactjs.org"
