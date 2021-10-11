080f0f3661975322b1ca15dc2e46f8fd77c47e88
aengun <57395045+aengun@users.noreply.github.com>
2021-10-11 22:06:54 +0900

    refactor: footer 스타일 변경 (#8)
    * refactore: footer 스타일 변경
    
    * refactor: footer 디자인 재수정
    
    * refactor: 스타일 분리 재설정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,63 +1,42 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
-import { useStyles } from './style';
-import FooterProfile from './FooterProfile';
+import { AppBar, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { GitHub } from '@material-ui/icons';
 
-interface Profile {
-  name: string;
-  position: string;
-  instagramAddress: string;
-  githubAddress: string;
-}
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    marginTop: 10,
+  },
+  appBottomBar: {
+    top: 'auto',
+    bottom: 0,
+    height: '100%',
+    marginTop: 10,
+  },
+  grid: {
+    height: '100%',
+  },
+});
 
 export default function Footer() {
   const classes = useStyles();
 
-  const profileList: Profile[] = [
-    {
-      name: '김건영',
-      position: 'developer',
-      instagramAddress: 'https://www.instagram.com/aengun_/',
-      githubAddress: 'https://github.com/aengun',
-    },
-    {
-      name: '박영운',
-      position: 'developer',
-      instagramAddress: 'https://instagram.com/_youngwoon/',
-      githubAddress: 'https://github.com/youngpark17',
-    },
-    {
-      name: '이주형',
-      position: 'developer',
-      instagramAddress: 'https://instagram.com/joooooooohy/',
-      githubAddress: 'https://github.com/junam2',
-    },
-  ];
-
   return (
     <div className={classes.root}>
-      <Grid container direction="column" spacing={2}>
-        <Grid item>
-          <Grid container direction="row" spacing={2} className={classes.grid}>
-            <Grid item xs={3}>
-              <Grid container direction="column">
-                <Grid item className={classes.profileGridItem} color="primary">
-                  <Typography variant="body1">고객센터 : 1588-XXXX</Typography>
-                </Grid>
-                <Grid item className={classes.profileGridItem}>
-                  <Typography variant="body1">주소 : none</Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-            {profileList.map((row: Profile, index) => {
-              return <FooterProfile key={index} {...row} />;
-            })}
+      <AppBar position="relative" className={classes.appBottomBar}>
+        <Grid container alignContent="center" className={classes.grid}>
+          <Grid item xs={4}>
+            <GitHub />
           </Grid>
+          <Grid item xs={4}>
+            <Typography variant="h6">contact : </Typography>
+          </Grid>
+          <Grid item xs={4}></Grid>
         </Grid>
-      </Grid>
-      <Grid item>
-        <Typography variant="subtitle2">© 2021 All Rights Reserved</Typography>
-      </Grid>
+      </AppBar>
     </div>
   );
 }
