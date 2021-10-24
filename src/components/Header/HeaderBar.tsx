a7afbe63ad1dbda11debc86b56ab6dca0da70221
youngUnPark <mduddns@naver.com>
2021-10-24 22:31:23 +0900

    feat: kakao 로그인 요청

--- src/components/Header/HeaderBar.tsx
+++ src/components/Header/HeaderBar.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import { createStyles, makeStyles, Theme, withStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import HeaderBarMenu from './HeaderBarMenu';
-import TextField from '@material-ui/core/TextField';
+import LoginButton from './LoginButton';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -58,7 +57,7 @@ export default function HeaderBar() {
           <Typography variant="h6" className={classes.title}>
             Give Me Money
           </Typography>
-          <img src="https://developers.kakao.com/tool/resource/static/img/button/login/simple/ko/kakao_login_small.png" />
+          <LoginButton></LoginButton>
         </Toolbar>
       </AppBar>
     </div>
