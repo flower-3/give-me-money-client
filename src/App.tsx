a41dcaeffd625b37d5de908c80a21d0f9db0f414
junam2 <speedlin4584@gmail.com>
2021-11-03 21:32:17 +0900

    refactor: App.tsx modal import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import HeaderBar from './components/HeaderBar';
 import GrantTable from './components/GrantTable';
 import Footer from './components/Footer/Footer';
-import Modal from './components/Modal/Modal';
 
 function App() {
   return (
