4dd4434b4b1afeef155795bae006e371f9b7e042
youngUnPark <mduddns@naver.com>
2021-11-04 23:05:15 +0900

    feat: 로그인 유지 로직 수정

--- src/components/Header/LoginButton.tsx
+++ src/components/Header/LoginButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import loginImage from '../../resources/images/kakao_login.png';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
@@ -6,7 +6,7 @@ type Props = {
   visibility: any;
 };
 
-const useStyles = makeStyles<Theme, Props>((theme) =>
+const useStyles = makeStyles<Theme, Props>(
   createStyles({
     logginButton: {
       visibility: ({ visibility }) => visibility,
@@ -14,15 +14,25 @@ const useStyles = makeStyles<Theme, Props>((theme) =>
   }),
 );
 const authenticateKakaoUser = (): void => {
+  //개발서버
   location.href =
     'https://kauth.kakao.com/oauth/authorize?client_id=0b19f85eee82ee6d30e24d4516cfb359&redirect_uri=http://34.83.199.174:8080/api/v1/auth/kakao/callback&response_type=code';
+
+  /*
+   로컬
+  location.href =
+    'https://kauth.kakao.com/oauth/authorize?client_id=0b19f85eee82ee6d30e24d4516cfb359&redirect_uri=http://127.0.0.1:8080/api/v1/auth/kakao/callback&response_type=code';
+  */
+};
+
+const ifBearExists = (): boolean => {
+  if (document.cookie.toString().indexOf('Bearer') > 0) {
+    return false;
+  }
+  return true;
 };
 export default function LoginButton() {
-  let isHidden = true;
-  if (document.referrer === '') {
-    isHidden = false;
-  }
-  const props: Props = { visibility: isHidden ? 'hidden' : 'visible' };
+  const props: Props = { visibility: ifBearExists() ? 'hidden' : 'visible' };
   const styles = useStyles(props);
   return (
     <div className={styles.logginButton}>
