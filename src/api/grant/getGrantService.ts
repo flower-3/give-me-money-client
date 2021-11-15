0af16600dbdc31ef6a8ce32fc9d55841d76bb66d
youngUnPark <mduddns@naver.com>
2021-11-15 23:46:40 +0900

    feat: 서버 api변경에 따른 uri변경

--- src/api/grant/getGrantService.ts
+++ src/api/grant/getGrantService.ts
@@ -1,14 +1,14 @@
 import useGrant from '../../hooks/useGrant';
 
 function getGrantServiceList(page: number, perPage: number) {
-  return useGrant('/api/v1/gov24/v1/serviceList', {
+  return useGrant('/api/v1/gov24/serviceList', {
     page: page,
     perPage: perPage,
   });
 }
 
 function getGrantServiceDetail(serviceId: string) {
-  return useGrant('/api/v1/gov24/v1/serviceDetail', {
+  return useGrant('/api/v1/gov24/serviceDetail', {
     serviceId: serviceId,
   });
 }
