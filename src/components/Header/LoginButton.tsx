db1387776e84f0e1de794d5f083cfdb3efae07dc
youngUnPark <mduddns@naver.com>
2021-11-28 21:04:55 +0900

    feat: 쿠키값에 따른 로그인 유지 처리

--- src/components/Header/LoginButton.tsx
+++ src/components/Header/LoginButton.tsx
@@ -20,20 +20,24 @@ const getCookie = function (name: string): string | null {
   const value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
   return value ? value[2] : null;
 };
+/*
+ * 서버에서 내려주는 쿠키에 있는 Bearer 토큰의 경우 HttpOnly 이므로
+ * 사용자의 세션을 체크할 수 있는 별도의 isLoggined 쿠키를 두어 로그인 체크
+ **/
 const setLocalStorageIfUserLoggined = (): void => {
   const URLSearch = new URLSearchParams(location.search);
-  if (URLSearch.get('success') === '1' || getCookie('isLogined') === '1') {
-    setCookie('isLogined', '1', 1);
+  if (URLSearch.get('success') === '1' || getCookie('isLoggined') === '1') {
+    setCookie('isLoggined', '1', 60 * 60 * 24);
   }
 };
 
 const authenticateKakaoUser = (): void => {
   localStorage.clear();
   //개발서버
-  // location.href =
-  //   'https://kauth.kakao.com/oauth/authorize?client_id=0b19f85eee82ee6d30e24d4516cfb359&redirect_uri=http://34.83.199.174:8080/api/v1/auth/kakao/callback&response_type=code';
-  document.location.href =
-    'https://kauth.kakao.com/oauth/authorize?client_id=0b19f85eee82ee6d30e24d4516cfb359&redirect_uri=http://127.0.0.1:8080/api/v1/auth/kakao/callback&response_type=code';
+  location.href =
+    'https://kauth.kakao.com/oauth/authorize?client_id=0b19f85eee82ee6d30e24d4516cfb359&redirect_uri=http://34.83.199.174:8080/api/v1/auth/kakao/callback&response_type=code';
+  // document.location.href =
+  //   'https://kauth.kakao.com/oauth/authorize?client_id=0b19f85eee82ee6d30e24d4516cfb359&redirect_uri=http://127.0.0.1:8080/api/v1/auth/kakao/callback&response_type=code';
 };
 
 interface Props {
@@ -42,7 +46,7 @@ interface Props {
 
 export default function LoginButton() {
   setLocalStorageIfUserLoggined();
-  const isVisible = getCookie('isLogined') === '1' ? false : true;
+  const isVisible = getCookie('isLoggined') === '1' ? false : true;
   const styles = useStyles({ isVisible });
   return (
     <div className={styles.logginButton}>
